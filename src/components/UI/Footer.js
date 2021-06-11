9a14b72668f79219cedc799176166556a6984d1b
Your Name <yashasvig43@gmail.com>
2021-06-10 19:57:27 -0500

    Initial comment

---
+++ src/components/UI/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './additional.css';
+const Footer = () => {
+    return (
+     <h1 className="copy">React Project Expense Manager | 2021 |<a href="yghere.github.io"> Yashasvi Goswami</a></h1>
+    );
+};
+  export default Footer;
